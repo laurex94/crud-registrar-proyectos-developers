7ffecdae2f32814085fd7e5e9a7c131c57c69c4b
Laurence Marcano <laurencejose94@gmail.com>
2023-01-11 21:16:27 -0400

    añadidos metodos a los resolver de projects

--- src/projects/projects.resolver.ts
+++ src/projects/projects.resolver.ts
@@ -1,4 +1,39 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Resolver, Mutation, Args } from '@nestjs/graphql';
+import {
+  GetProjectDto,
+  GetOneProjectInput,
+  CreateProjectInput,
+  UpdateProjectInput,
+  DeleteProjectInput,
+} from './dto/projects.dto';
+import { ProjectsService } from './projects.service';
 
 @Resolver()
-export class ProjectsResolver {}
+export class ProjectsResolver {
+  constructor(private projectsService: ProjectsService) {}
+
+  @Query(() => [GetProjectDto])
+  async getAllProjects() {
+    return this.projectsService.findAll();
+  }
+
+  @Query(() => GetProjectDto)
+  async getProjectById(@Args('input') input: GetOneProjectInput) {
+    return this.projectsService.findOne(input.id);
+  }
+
+  @Mutation(() => GetProjectDto)
+  async createProject(@Args('input') input: CreateProjectInput) {
+    return this.projectsService.create(input);
+  }
+
+  @Mutation(() => GetProjectDto)
+  async updateProject(@Args('input') input: UpdateProjectInput) {
+    return this.projectsService.update(input);
+  }
+
+  @Mutation(() => GetProjectDto)
+  async DeleteProject(@Args('input') input: DeleteProjectInput) {
+    return this.projectsService.remove(input.id);
+  }
+}
