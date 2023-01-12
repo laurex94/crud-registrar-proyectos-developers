fdd4586678ac82b10755e0b19b98878e210aa2c5
Laurence Marcano <laurencejose94@gmail.com>
2023-01-11 21:08:35 -0400

    Añadidos servicios a modulo de proyectos

---
+++ src/projects/dto/projects.dto.ts
@@ -0,0 +1,85 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsEmail,
+  IsNumber,
+  IsPositive,
+} from 'class-validator';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { statusProject } from '../entities/projects.entity';
+
+@ObjectType()
+export class GetProjectDto {
+  @Field()
+  @IsNumber()
+  @IsPositive()
+  readonly id: number;
+
+  @Field()
+  @IsString()
+  readonly name: string;
+
+  @Field()
+  @IsString()
+  readonly description: string;
+
+  @Field()
+  @IsString()
+  readonly status: statusProject;
+}
+
+@InputType()
+export class GetOneProjectInput {
+  @Field()
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
+  readonly id: number;
+}
+
+@InputType()
+export class CreateProjectInput {
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @Field()
+  @IsString()
+  readonly description: string;
+
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  readonly status: statusProject;
+}
+
+@InputType()
+export class UpdateProjectInput {
+  @Field()
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
+  readonly id: number;
+
+  @Field()
+  @IsString()
+  readonly name: string;
+
+  @Field()
+  @IsString()
+  readonly description: string;
+
+  @Field()
+  @IsString()
+  readonly status: statusProject;
+}
+
+@InputType()
+export class DeleteProjectInput {
+  @Field()
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
+  readonly id: number;
+}
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Project, statusProject } from './entities/projects.entity';
+
+import { CreateProjectInput, UpdateProjectInput } from './dto/projects.dto';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  private counterId = 1;
+  private projects: Project[] = [
+    {
+      id: 1,
+      name: 'Proyecto 1',
+      description: 'Descripcion proyecto 1',
+      status: statusProject.available,
+    },
+  ];
+
+  async findAll() {
+    const projects = this.projects;
+    if (!projects) {
+      throw new NotFoundException(`Developers not found`);
+    }
+    return projects;
+  }
+
+  async findOne(id: number) {
+    const project = this.projects.find((data) => data.id === id);
+    if (!project) {
+      throw new NotFoundException(`Developer #${id} not found`);
+    }
+    return project;
+  }
+
+  async create(data: CreateProjectInput) {
+    this.counterId = this.counterId + 1;
+    const newProject = {
+      id: this.counterId,
+      ...data,
+    };
+    this.projects.push(newProject);
+    return newProject;
+  }
+
+  async update(changes: UpdateProjectInput) {
+    const project = this.findOne(changes.id);
+    const index = this.projects.findIndex((data) => data.id === changes.id);
+    this.projects[index] = {
+      ...project,
+      ...changes,
+    };
+    return this.projects[index];
+  }
+
+  async remove(id: number) {
+    const project = this.findOne(id);
+    const index = this.projects.findIndex((data) => data.id === id);
+    if (index === -1) {
+      throw new NotFoundException(`Developer #${id} not found`);
+    }
+    this.projects.splice(index, 1);
+    return project;
+  }
+}
